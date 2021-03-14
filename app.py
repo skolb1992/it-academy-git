7db311daed89fc1af0f5bd543558145033857d23
Sergey <s.kolb1992@yandex.ru>
2021-03-15 01:49:45 +0300

    added input: name surname and print: name surname

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 print('I’m planning to learn git conflicts resolution')
+
+name = input('Please enter your name:')
+
+surname = input('Please enter your surname:')
+
+print('Hi, {} {}!'.format(name, surname))
